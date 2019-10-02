52134e01e1f276d5479e49c0d40278065aadde96
Mateus Dal Bianco <mateus@dlbn.co>
2019-10-02 12:11:01 +0300

    initial commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,41 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react';
 
-import ExampleComponent from 'react-moon'
+import Moon from 'react-moon';
 
-export default class App extends Component {
-  render () {
-    return (
+let interval;
+
+const App = () => {
+  const [phase, setPhase] = useState(0);
+  const play = () => {
+    interval = window.setInterval(() => {
+      setPhase(phase => {
+        const newValue = phase < 1 ? phase + 0.005 : 0;
+        return newValue;
+      });
+    }, 50);
+  };
+  const pause = () => {
+    window.clearInterval(interval);
+  };
+  return (
+    <div>
+      <Moon size={320} phase={phase} />
+      phase: {phase.toFixed(2)}
+      <br />
+      <input
+        type="range"
+        min={0}
+        max={1}
+        value={phase}
+        step={0.001}
+        onChange={e => setPhase(e.target.valueAsNumber)}
+      />
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <button onClick={play}>▶️</button>
+        <button onClick={pause}>⏸️</button>
       </div>
-    )
-  }
-}
+    </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,92 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled, { css } from 'styled-components';
 
-import styles from './styles.css'
+const Wrapper = styled.div`
+  display: flex;
+  position: relative;
+  transition: transform 1s;
+  border-radius: 50%;
+  ${({ size, rotation, darkColor }) => css`
+    background-color: ${darkColor};
+    width: ${size}px;
+    height: ${size}px;
+    transform: rotate(${rotation}rad);
+  `}
+`;
 
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
-  }
+const Half = styled.div`
+  width: 100%;
+  overflow: hidden;
+  opacity: ${({ isVisible }) => (isVisible ? 1 : 0)};
+  ${({ invert }) =>
+    invert &&
+    css`
+      transform: scaleX(-1);
+    `}
+`;
 
-  render() {
-    const {
-      text
-    } = this.props
+const getNormalizedPhase = phase => (phase <= 0.5 ? phase : 1 - phase);
 
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
-  }
-}
+const getRotationRad = phase => {
+  const normalizedPhase = getNormalizedPhase(phase);
+  const rad = (Math.PI * normalizedPhase) / 0.5;
+  return rad;
+};
+
+const Difference = styled.div`
+  position: absolute;
+  z-index: 1;
+  left: 50%;
+  background: ${({ phase, darkColor, lightColor }) =>
+    0.5 / getNormalizedPhase(phase) > 2 ? darkColor : lightColor};
+  ${({ size, phase }) => css`
+    width: ${size}px;
+    height: ${size}px;
+    border-radius: 50%;
+    transform: translateX(-50%) rotateY(${getRotationRad(phase)}rad);
+  `}
+`;
+
+const Circle = styled.div`
+  ${({ size, lightColor }) => css`
+    width: ${size}px;
+    height: ${size}px;
+    background-color: ${lightColor};
+  `}
+  border-radius: 50%;
+`;
+
+const Moon = ({ size, rotation, darkColor, lightColor, phase, ...props }) => {
+  return (
+    <Wrapper darkColor={darkColor} rotation={rotation} size={size} {...props}>
+      <Half size={size} isVisible={phase <= 0.5}>
+        <Circle darkColor={darkColor} lightColor={lightColor} size={size} />
+      </Half>
+      <Difference
+        darkColor={darkColor}
+        lightColor={lightColor}
+        size={size}
+        phase={phase}
+      />
+      <Half size={size} invert isVisible={phase >= 0.5}>
+        <Circle darkColor={darkColor} lightColor={lightColor} size={size} />
+      </Half>
+    </Wrapper>
+  );
+};
+
+Moon.propTypes = {
+  size: PropTypes.number,
+  moon: PropTypes.object.isRequired,
+  rotation: PropTypes.number,
+};
+
+Moon.defaultProps = {
+  size: 80,
+  darkColor: 'black',
+  lightColor: 'white',
+  rotation: 0,
+};
+
+export default Moon;
